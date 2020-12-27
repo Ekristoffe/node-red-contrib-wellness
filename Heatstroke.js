db7bd813b2f4c0c6ca6965b12706c3193c68ceab
Chris <aidasan@hotmail.fr>
2020-12-27 15:00:00 +0900

    Minor updates

--- Heatstroke.js
+++ Heatstroke.js
@@ -6,38 +6,38 @@ module.exports = function (RED) {
 		const context = this.context();
         const node = this;
         this.name = config.name;
-		context.set("temperature", 20);
-		context.set("humidity", 50);
+		context.set('temperature', 20);
+		context.set('humidity', 50);
 
 		// Add a timeout to reset the status to green after x time (ms)
 		let timeoutStatus;
 
-		node.status({fill: "green", shape: "dot", text: ""});
+		node.status({fill: 'green', shape: 'dot', text: ''});
 
         node.on('input', function (msg) {
 			const _topic = msg.topic.toLowerCase();
-			let _temperature = context.get("temperature") || 20;
-			let _humidity = context.get("humidity") || 20;
+			let _temperature = context.get('temperature') || 20;
+			let _humidity = context.get('humidity') || 20;
 			switch(_topic) {
-				case "temperature":
-				case "temp":
+				case 'temperature':
+				case 'temp':
 					_temperature = parseFloat(msg.payload);
 					clearTimeout(timeoutStatus);
-					node.status({fill: "blue", shape: "dot", text: "temperature updated"});
+					node.status({fill: 'blue', shape: 'dot', text: 'temperature updated'});
 					timeoutStatus = setTimeout(() => {
-						node.status({fill: "green", shape: "dot", text: ""});
+						node.status({fill: 'green', shape: 'dot', text: ''});
 					}, 1000);
-					context.set("temperature", _temperature);
+					context.set('temperature', _temperature);
 					break;
-				case "humidity":
-				case "hum":
+				case 'humidity':
+				case 'hum':
 					_humidity = parseFloat(msg.payload);
 					clearTimeout(timeoutStatus);
-					node.status({fill: "blue", shape: "dot", text: "humidity updated"});
+					node.status({fill: 'blue', shape: 'dot', text: 'humidity updated'});
 					timeoutStatus = setTimeout(() => {
-						node.status({fill: "green", shape: "dot", text: ""});
+						node.status({fill: 'green', shape: 'dot', text: ''});
 					}, 1000);
-					context.set("humidity", _humidity);
+					context.set('humidity', _humidity);
 					break;
 			}
 
@@ -47,5 +47,5 @@ module.exports = function (RED) {
         });
     }
 
-    RED.nodes.registerType("Heatstroke-Index", HeatstrokeIndex);
+    RED.nodes.registerType('Heatstroke-Index', HeatstrokeIndex);
 };
